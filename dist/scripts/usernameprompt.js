ec26a27029893fb660646b9901b71f2491240813
Kristen Rieger <karieger1@gmail.com>
2015-06-06 12:08:14 -0500

    everything working

--- dist/scripts/usernameprompt.js
+++ dist/scripts/usernameprompt.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
+matthew = {};
+	// $("#messageBox").click (function signIn() {
+	// 	 alert("In order to participate in the chat you must type in a user name:");
+	// 	 var textInput = prompt	
 
-//prompt for username upon page load
+	// });
+
 swal ({ 
-	title: "Welcome to the Cat Chat experience.",   
-	text: "Type a cat-related username below:",   
+	title: "Welcome to Number One Chatroom!",   
+	text: "Type a username below. Make it good.",   
 	type: "input",   
 	showCancelButton: true,   
 	closeOnConfirm: false,   
@@ -12,6 +17,9 @@ swal ({
 	imageURL: "http://graphics8.nytimes.com/images/2012/10/20/health/20well-cheeto2/20well-cheeto2-tmagArticle.jpg"
 	}, 
 	function(name){ 
+		console.log(typeof name);
+		$('span').append(name);
+		matthew.nombre = name;
 	  	if (name === false) 
 		return false;      
 		if (name === "") {     
@@ -19,51 +27,80 @@ swal ({
 			return false   }
 			swal("Well, alright...", "Your amazing username is: " + name, "success"); });
 		
+//username.name
 
-
-
-//populate window with user's input on submit button click
-	$("#messageBox").keydown(function(event) {
-		if(event.keyCode==13) {
-			$(".btn").trigger("click");
-		}
+//   $.post("https://the-living-room.herokuapp.com/login", {
+// 		user: "name",
+// 		chatroom: "The Living Room",
+// 		message: " ", /* username */
+// 		} );
 	
+// });
 
-	$('.btn').click(fruit);
 
-		function fruit(event){
-
-	        event.preventDefault();
-
-	        console.log($('#messageBox').val());
-
-	        $.post('https://cat-chat-1.herokuapp.com/messages', 
-	        	{message: $('#messageBox').val(), 
-	        	user: name, chatroom: "The Living Room"}, apple);
-
+function apple(){console.log(matthew.nombre);$.get('https://cat-chat-1.herokuapp.com/messages', function(z)
+		{	yell = z[z.length-1].message; 
+        	talky();
+        	for(var p = 0;p<z.length;p++){$('#displayWindow').append(z[p].user + ":" + z[p].message + "<br/>");
+    		}
+        	$('#displayWindow').scrollTop($('#displayWindow')[0].scrollHeight);
+        });
+}
 		
+	apple();
+	
+var banana = "house";
 
-		$('#messageBox').val(" ");
+$('#fred').click(fruit);
+function fruit(event){
+	event.preventDefault();
+	console.log($('#messageBox').val());
+	$.post('https://cat-chat-1.herokuapp.com/messages', 
+		{message: $('#messageBox').val(), user: matthew.nombre, chatroom: "The Living Room"}, apple);
+		 $('#messageBox').val('');
+};
+	
+function keith(){
+	$.get('https://cat-chat-1.herokuapp.com/stats', function(data){ 
+		var mpc = data.mostPopularChatroom.name;
+		var ttu = data.topTenUsers;
+		var rau = data.recentlyActiveUsers;
+
+
+		$('#stats').append("<div class='stats'>Most Popular Chatroom</div><br/>"+mpc+
+						"<br/><br/><div class='stats'>Top Ten Users</div><br/>");
+
+		for(var m=0;m<ttu.length;m++){
+		$('#stats').append(ttu[m].user+"<br/>");					 
+		}	
+
+		$('#stats').append("<br/>Recently Active Users<br/><br/>");
+
+		for(var i=0;i<rau.length;i++){
+		$('#stats').append(rau[i]+"<br/>");
 		}
 
+	});
+
+};
 
 
-	function apple(){$.get('https://cat-chat-1.herokuapp.com/messages', 
-		function(z)
-			{
-	        	for(var p = 0;p<z.length;p++)
-	        		{$('#displayWindow').append(z[p].user + ":" + z[p].message + "<br/>");
-	    		}
-	        	$('#displayWindow').scrollTop($('#displayWindow')[0].scrollHeight);
-	        });
-	}
-			
-		apple();
-	
+keith();
 
 
 
 
-  });
+var yell= "i hate you so much"
+function talky(){
+	var text = yell;
+	text = encodeURIComponent(text);
+	var url = "https://translate.google.com/translate_tts?ie=UTF-&&q=" + text + "&tl=en";
+	$('audio').attr('src',url).get(0).play();
+}
+talky();
+
+
+
+
 
 }); //very end
